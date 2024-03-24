533a839cfa2ff3f4ef5b70383759df40d2c635c6
ParamonovaE <128289272+ParamonovaE@users.noreply.github.com>
2024-03-25 01:22:50 +0300

    Add files via upload

---
+++ task2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Employee (
+    employee_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    department VARCHAR(100),
+    chief_id INTEGER NULL REFERENCES Employee(employee_id)
+);
