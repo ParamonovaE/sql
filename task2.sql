f6cad04460c786c15c28dee54e7fbed00379e9a3
ParamonovaE <128289272+ParamonovaE@users.noreply.github.com>
2024-03-25 01:26:53 +0300

    Update task2.sql

--- task2.sql
+++ task2.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Employee (
     employee_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    department VARCHAR(100),
+    department VARCHAR(100) NOT NULL,
     chief_id INTEGER NULL REFERENCES Employee(employee_id)
 );
